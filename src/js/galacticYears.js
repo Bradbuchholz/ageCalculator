00f905f574456306ee6345e818ecb682cb944893
Bradbuchholz <bradbuchholz@gmail.com>
2022-05-21 16:30:49 -0700

    frustrated, going for a walk

--- src/js/galacticYears.js
+++ src/js/galacticYears.js
@@ -2,4 +2,16 @@ export default class galacticYears {
   constructor(age) {
     this.age = age;
   }
+
+  cacl(planet) {
+    let ageMercury;
+    let ageVenus;
+    let ageMars;
+    let ageJupiter;
+    switch(planet) {
+      case "Mercury":
+        ageMercury = parseFloat((this.age/.24).toFixed(2));
+        return ageMercury;
+    }
+  }
 }
