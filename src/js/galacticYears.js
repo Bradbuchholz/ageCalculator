f06cf7dac986a4dc184c057c61d2b3374a10a240
Bradbuchholz <bradbuchholz@gmail.com>
2022-05-21 22:13:18 -0700

    second test passed

--- src/js/galacticYears.js
+++ src/js/galacticYears.js
@@ -2,5 +2,6 @@ export default class galacticYears {
   constructor(age, lifeExpectancy){
     this.age = age;
     this.lifeExpectancy = lifeExpectancy;
+    this.mercAge = Math.floor(this.age/.24);
   }
 };
