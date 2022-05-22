5fe8b931cbac28fe32d52c220bad0732e11d5a3e
Bradbuchholz <bradbuchholz@gmail.com>
2022-05-22 00:07:34 -0700

    test 7 passed + README.md

--- __tests__/test.js
+++ __tests__/test.js
@@ -27,5 +27,11 @@ test('should correctly return expected years left to live on each planet', () =>
   expect(years.marsYearsLeftOrOver).toEqual(24);
   expect(years.jupYearsLeftOrOver).toEqual(3);
   });
-test('')
+test('should correctly return years lived over expected on each planet', () => {
+  years = new galacticYears(105, 83)
+  expect(years.mercYearsLeftOrOver).toEqual(91);
+  expect(years.venYearsLeftOrOver).toEqual(35);
+  expect(years.marsYearsLeftOrOver).toEqual(11);
+  expect(years.jupYearsLeftOrOver).toEqual(1);
+  });
 });
